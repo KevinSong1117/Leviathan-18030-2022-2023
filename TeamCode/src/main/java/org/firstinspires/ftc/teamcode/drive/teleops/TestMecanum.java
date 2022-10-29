8ba42f2989cecb6a36374d596a06698f68ef8b16
KevinSong1117 <68066592+KevinSong1117@users.noreply.github.com>
2022-10-29 16:02:09 -0500

    build day
    fk this shi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teleops/TestMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teleops/TestMecanum.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="mecanumDrive1", group="teleop")
 public class TestMecanum extends LinearOpMode {
@@ -14,8 +15,13 @@ public class TestMecanum extends LinearOpMode {
         DcMotor bL = hardwareMap.dcMotor.get("bL");
         DcMotor fR = hardwareMap.dcMotor.get("fR");
         DcMotor bR = hardwareMap.dcMotor.get("bR");
-        DcMotor lR = hardwareMap.dcMotor.get("lR");
-        DcMotor lL = hardwareMap.dcMotor.get("lL");
+
+        DcMotor lI = hardwareMap.dcMotor.get("lI");
+        DcMotor rI = hardwareMap.dcMotor.get("rI");
+        Servo lL = hardwareMap.servo.get("lL");
+        Servo rL = hardwareMap.servo.get("rL");
+
+        lI.setDirection(DcMotor.Direction.REVERSE);
 
         fR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         fL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -26,8 +32,6 @@ public class TestMecanum extends LinearOpMode {
         bR.setDirection(DcMotor.Direction.FORWARD);
         fL.setDirection(DcMotor.Direction.REVERSE);
         bL.setDirection(DcMotor.Direction.FORWARD);
-
-        lL.setDirection(DcMotor.Direction.REVERSE);
 
         BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
@@ -43,9 +47,6 @@ public class TestMecanum extends LinearOpMode {
             double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
 
-            double liftR = -gamepad2.right_stick_y;
-            double liftL = -gamepad2.left_stick_y;
-
             double botHeading = -imu.getAngularOrientation().firstAngle;
 
             double rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
@@ -58,14 +59,25 @@ public class TestMecanum extends LinearOpMode {
             double backRightPower = (rotY + rotX - rx) / denominator;
 
 
-            lR.setPower(liftR);
-            lL.setPower(liftL);
+            fL.setPower (.1);//(frontLeftPower);
+            bL.setPower (.1);//(backLeftPower);
+            fR.setPower (.1);//(frontRightPower);
+            bR.setPower (.1);//(backRightPower);
 
-            fL.setPower(frontLeftPower);
-            bL.setPower(backLeftPower);
-            fR.setPower(frontRightPower);
-            bR.setPower(backRightPower);
-
+            if (gamepad1.a) {
+                lL.setPosition(1);
+                rL.setPosition(0);
+            }
+            if (gamepad1.b) {
+                lL.setPosition(0);
+                rL.setPosition(1);
+            }
+            while(gamepad2.left_bumper){
+                lI.setPower(.5);
+                rI.setPower(.5);
+            }
+            lI.setPower(0);
+            rI.setPower(0);
 
             telemetry.addData("FL encoder value:", fL.getCurrentPosition());
             telemetry.addData("FR encoder value:", fR.getCurrentPosition());
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teleops/Testing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teleops/Testing.java
@@ -3,19 +3,39 @@ package org.firstinspires.ftc.teamcode.drive.teleops;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="testing", group="test")
 public class Testing extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+        DcMotor fL = hardwareMap.dcMotor.get("fL");
+        DcMotor bL = hardwareMap.dcMotor.get("bL");
+        DcMotor fR = hardwareMap.dcMotor.get("fR");
+        DcMotor bR = hardwareMap.dcMotor.get("bR");
 
-        DcMotor lR = hardwareMap.dcMotor.get("lR");
+        DcMotor lI = hardwareMap.dcMotor.get("lI");
+        DcMotor rI = hardwareMap.dcMotor.get("rI");
+        Servo lO = hardwareMap.servo.get("lO");
+        Servo rO = hardwareMap.servo.get("rO");
         DcMotor lL = hardwareMap.dcMotor.get("lL");
+        DcMotor rL = hardwareMap.dcMotor.get("rL");
+        CRServo fI = hardwareMap.get(CRServo.class, "fI");
+        CRServo bI = hardwareMap.get(CRServo.class, "bI");
+        lI.setDirection(DcMotor.Direction.REVERSE);
 
+        fR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        lL.setDirection(DcMotor.Direction.REVERSE);
+        fR.setDirection(DcMotor.Direction.FORWARD);
+        bR.setDirection(DcMotor.Direction.FORWARD);
+        fL.setDirection(DcMotor.Direction.REVERSE);
+        bL.setDirection(DcMotor.Direction.FORWARD);
 
         BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
@@ -27,15 +47,59 @@ public class Testing extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
 
-            double liftR = -gamepad2.right_stick_y;
-            double liftL = -gamepad2.left_stick_y;
+            double botHeading = -imu.getAngularOrientation().firstAngle;
+
+            double rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
+            double rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
 
 
+            fL.setPower(frontLeftPower);
+            bL.setPower(backLeftPower);
+            fR.setPower(frontRightPower);
+            bR.setPower(backRightPower);
 
-            lR.setPower(liftR/3);
-            lL.setPower(liftL/3);
+            if (gamepad2.x) {
+                fI.setPower(.5);
+                bI.setPower(.5);
+            }
+            if (gamepad2.y) {
+                fI.setPower(0);
+                bI.setPower(0);
+            }
+            if (gamepad2.a) {
+                lO.setPosition(1);
+                rO.setPosition(0);
+            }
+            if (gamepad2.b) {
+                lO.setPosition(0);
+                rO.setPosition(1);
+            }
+            while(gamepad2.left_bumper){
+                lI.setPower(.5);
+                rI.setPower(.5);
+            }
+            lI.setPower(0);
+            rI.setPower(0);
 
+            double liftP = -gamepad2.left_stick_y;
+            lL.setPower(y/2);
+            rL.setPower(y/2);
+
+            telemetry.addData("FL encoder value:", fL.getCurrentPosition());
+            telemetry.addData("FR encoder value:", fR.getCurrentPosition());
+            telemetry.addData("BL encoder value:", bL.getCurrentPosition());
+            telemetry.addData("BR encoder value:", bR.getCurrentPosition());
+            telemetry.update();
         }
     }
 }
