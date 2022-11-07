ca20cffdac7a37b42a30a3d0133d7348444c2a88
KevinSong1117 <68066592+KevinSong1117@users.noreply.github.com>
2022-11-07 11:46:00 -0600

    This morning
    Fixed teleop for wesday

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teleops/Testingeachmotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teleops/Testingeachmotor.java
@@ -51,34 +51,33 @@ public class Testingeachmotor extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y;
-            double x = gamepad1.left_stick_x * 1.1;
+            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double x = gamepad1.left_stick_x; // Counteract imperfect strafing multiply by 1.1 if not work
             double rx = gamepad1.right_stick_x;
 
-            double botHeading = -imu.getAngularOrientation().firstAngle;
-
-            double rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
-            double rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
-
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (rotY + rotX + rx) / denominator;
-            double backLeftPower = (rotY - rotX + rx) / denominator;
-            double frontRightPower = (rotY - rotX - rx) / denominator;
-            double backRightPower = (rotY + rotX - rx) / denominator;
-
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
 
             fL.setPower(frontLeftPower);
             bL.setPower(backLeftPower);
             fR.setPower(frontRightPower);
             bR.setPower(backRightPower);
+
             if (gamepad2.x) {
                 fI.setPower(1);
+                bI.setPower(-1);
+            }else if (gamepad2.y) {
+                fI.setPower(-1);
                 bI.setPower(1);
-            }
-            if (gamepad2.y) {
-                fI.setPower(0);
-                bI.setPower(0);
-            }
+            }else{fI.setPower(0);
+                bI.setPower(0);}
+
             double liftp = -gamepad2.left_stick_y;
             lL.setPower(liftp/2);
             rL.setPower(liftp/2);
@@ -90,14 +89,14 @@ public class Testingeachmotor extends LinearOpMode {
                 lO.setPosition(0);
                 rO.setPosition(1);
             }
-            if(gamepad2.left_bumper){
+            if(gamepad1.left_bumper){
                 lI.setPower(-.5);
                 rI.setPower(.5);
-            }
-            else{
-                lI.setPower(0);
-                rI.setPower(0);
-            }
+            }else if(gamepad1.right_bumper)
+            {lI.setPower(.5);
+                rI.setPower(-.5);}
+            else{lI.setPower(0);
+                rI.setPower(0);}
 
 
             telemetry.addData("FL encoder value:", fL.getCurrentPosition());
